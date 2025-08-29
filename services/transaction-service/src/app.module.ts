8350e9693db5ceed6f9fe2a19e5a5555920cd11d
Khumo Mogorosi <khumomogorosi@gmail.com>
2025-08-29 20:13:56 +0200

    feat(transaction): implement transaction controller

--- services/transaction-service/src/app.module.ts
+++ services/transaction-service/src/app.module.ts
@@ -1,12 +1,29 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+
 import { TransactionModule } from './transaction/transaction.module';
 import { KafkaModule } from './kafka/kafka.module';
+import { Transaction } from './transaction/entities/transaction.entity';
 
 @Module({
-  imports: [TransactionModule, KafkaModule],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        url: configService.get<string>('DATABASE_URL'),
+        entities: [Transaction],
+        synchronize: true, // Only for development
+        logging: true,
+      }),
+      inject: [ConfigService],
+    }),
+    TransactionModule,
+    KafkaModule,
+  ],
 })
 export class AppModule {}
